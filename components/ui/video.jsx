61eee1fdd374fe7f2a6c1983d038df464562dd8c
Mustafa <mrmrmustafag77@gmail.com>
2024-02-18 21:49:34 +0300

    hide play button on mobile

--- components/ui/video.jsx
+++ components/ui/video.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useState, useRef } from "react";
 
 export default function Video({ width, height, src, className }) {
@@ -24,26 +23,31 @@ export default function Video({ width, height, src, className }) {
       <video
         width={width}
         height={height}
-        className={className}
+        className={`rounded-[20px] ${className}`}
         controls
         preload="none"
-        poster="/assets/about-01.jpg">
+        poster="/assets/about-01.jpg"
+        ref={videoRef}>
         <source src={src} type="video/mp4" />
       </video>
 
-      <button className="absolute z-20 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="5em"
-          height="5em"
-          viewBox="0 0 24 24"
-          className="drop-shadow-lg">
-          <path
-            fill="#e5e5e5"
-            d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2m-2 14.5v-9l6 4.5z"
-          />
-        </svg>
-      </button>
+      {!isPlaying && (
+        <button
+          className="absolute z-20 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 hidden sm:block"
+          onClick={togglePlay}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="5em"
+            height="5em"
+            viewBox="0 0 24 24"
+            className="drop-shadow-lg">
+            <path
+              fill="#e5e5e5"
+              d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2m-2 14.5v-9l6 4.5z"
+            />
+          </svg>
+        </button>
+      )}
     </div>
   );
 }
