a38c39d4bdc7960a5d5e0528674e4e93f86169e4
Mustafa <mrmrmustafag77@gmail.com>
2024-02-22 21:44:36 +0300

    testing videos on iphone

--- components/ui/video.jsx
+++ components/ui/video.jsx
@@ -24,9 +24,6 @@ export default function Video({ width, height, src, className, poster = "" }) {
         width={width}
         height={height}
         className="rounded-[20px] w-full"
-        controls
-        preload="metadata"
-        poster={poster}
         ref={videoRef}
         onPlay={() => setIsPlaying(true)}
         onPause={() => setIsPlaying(false)}>
