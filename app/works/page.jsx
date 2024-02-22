82fce51fbff1f430beacf0f868fd94d1d9dd686a
Mustafa <mrmrmustafag77@gmail.com>
2024-02-22 15:42:11 +0300

    minimizing works page

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -1,11 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { categories, works } from "@/constants";
-import WorkCard from "@/components/cards/WorkCard";
-import Subheader from "@/components/shared/Subheader";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import PaginationComponent from "@/components/shared/PaginationComponent";
+import Works from "@/components/sections/Works";
 
 export default function Page() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -23,109 +19,18 @@ export default function Page() {
     setActiveType(type);
   }
 
-  function filterWorksByCategory(category, type) {
-    if (category === "all") {
-      return works.filter((work) => work.type === type);
-    } else {
-      return works.filter(
-        (work) => work.category === category && work.type === type
-      );
-    }
-  }
-
-  const renderWorkCards = () => {
-    const filteredWorks = filterWorksByCategory(activeCategory, activeType);
-
-    // Sort works by isFeatured property, placing featured works first
-    filteredWorks.sort((a, b) =>
-      a.isFeatured === b.isFeatured ? 0 : a.isFeatured ? -1 : 1
-    );
-
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-
-    return filteredWorks
-      .slice(startIndex, endIndex)
-      .map((work, index) => <WorkCard key={index} work={work} />);
-  };
-
-  const renderPagination = () => {
-    const filteredWorks = filterWorksByCategory(activeCategory, activeType);
-
-    return (
-      <PaginationComponent
-        totalItems={filteredWorks.length}
-        itemsPerPage={itemsPerPage}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-    );
-  };
-
   return (
     <div className="min-h-screen">
-      <div className="container">
-        <Subheader text="أعمالنا" />
-
-        {/* Main Tabs */}
-        <Tabs
-          dir="rtl"
-          defaultValue="all"
-          className="w-full flex flex-col items-center gap-10">
-          <TabsList className="rounded-full py-8 sm:py-10 px-2 w-full xs:w-auto justify-start xs:justify-center bg-gray-200 text-gray-600">
-            {categories.map((category) => (
-              <TabsTrigger
-                key={category.value}
-                value={category.value}
-                className="rounded-full p-3 sm:p-5"
-                onClick={() => handleCategoryChange(category.value)}>
-                {category.title}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-
-          {/* Secondary Tabs for Type */}
-          <Tabs
-            dir="rtl"
-            defaultValue="image"
-            className="w-full flex flex-col items-center gap-10">
-            <TabsList className="rounded-full py-7 justify-start xs:justify-center bg-gray-200 text-gray-600">
-              <TabsTrigger
-                className="p-3 rounded-full"
-                value="image"
-                onClick={() => handleTypeChange("image")}>
-                الصور
-              </TabsTrigger>
-
-              <TabsTrigger
-                className="p-3 rounded-full"
-                value="video"
-                onClick={() => handleTypeChange("video")}>
-                الفيديوهات
-              </TabsTrigger>
-            </TabsList>
-          </Tabs>
-          {/* Secondary Tabs for Type */}
-
-          {categories.map((category) => (
-            <TabsContent
-              key={category.value}
-              value={category.value}
-              className="w-full">
-              <div className="flex-wrapper">{renderWorkCards()}</div>
-
-              {renderWorkCards().length === 0 && (
-                <p className="text-body-semibold text-center">
-                  لم يتم العثور على أعمال
-                </p>
-              )}
-
-              {renderPagination()}
-            </TabsContent>
-          ))}
-        </Tabs>
-        {/* Main Tabs */}
-      </div>
+      <Works
+        activeCategory={activeCategory}
+        activeType={activeType}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        itemsPerPage={itemsPerPage}
+        handleCategoryChange={handleCategoryChange}
+        handleTypeChange={handleTypeChange}
+        isRoute
+      />
     </div>
   );
 }
--- components/sections/Works.jsx
+++ components/sections/Works.jsx
@@ -3,31 +3,78 @@ import ButtonComponent from "../shared/ButtonComponent";
 import Subheader from "../shared/Subheader";
 import { categories, works } from "@/constants";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import PaginationComponent from "../shared/PaginationComponent";
 
-export default function Works() {
-  function filterWorksByCategory(category) {
-    if (category === "all") {
-      return works;
+export default function Works({
+  activeCategory,
+  activeType,
+  currentPage,
+  setCurrentPage,
+  itemsPerPage,
+  handleCategoryChange,
+  handleTypeChange,
+  isRoute,
+}) {
+  function filterWorksByCategory(category, type) {
+    if (isRoute) {
+      if (category === "all") {
+        return works.filter((work) => work.type === type);
+      } else {
+        return works.filter(
+          (work) => work.category === category && work.type === type
+        );
+      }
     } else {
-      return works.filter((work) => work.category === category);
+      if (category === "all") {
+        return works;
+      } else {
+        return works.filter((work) => work.category === category);
+      }
     }
   }
 
-  const renderWorkCards = (category) => {
-    const filteredWorks = filterWorksByCategory(category);
+  function renderWorkCards(category) {
+    if (isRoute) {
+      const filteredWorks = filterWorksByCategory(activeCategory, activeType);
 
-    // Sort works by isFeatured property, placing featured works first
-    filteredWorks.sort((a, b) =>
-      a.isFeatured === b.isFeatured ? 0 : a.isFeatured ? -1 : 1
+      filteredWorks.sort((a, b) =>
+        a.isFeatured === b.isFeatured ? 0 : a.isFeatured ? -1 : 1
+      );
+
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+
+      return filteredWorks
+        .slice(startIndex, endIndex)
+        .map((work, index) => <WorkCard key={index} work={work} />);
+    } else {
+      const filteredWorks = filterWorksByCategory(category);
+
+      filteredWorks.sort((a, b) =>
+        a.isFeatured === b.isFeatured ? 0 : a.isFeatured ? -1 : 1
+      );
+
+      return filteredWorks
+        .slice(0, 6)
+        .map((work, index) => <WorkCard key={index} work={work} />);
+    }
+  }
+
+  function renderPagination() {
+    const filteredWorks = filterWorksByCategory(activeCategory, activeType);
+
+    return (
+      <PaginationComponent
+        totalItems={filteredWorks.length}
+        itemsPerPage={itemsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     );
-
-    return filteredWorks
-      .slice(0, 6)
-      .map((work, index) => <WorkCard key={index} work={work} />);
-  };
+  }
 
   return (
-    <section id="works">
+    <section id={isRoute ? "" : "/#works"}>
       <div className="container text-center">
         {/* Subheading */}
         <Subheader text="أعمالنا" className="mt-16" />
@@ -39,15 +86,50 @@ export default function Works() {
           defaultValue="all"
           className="w-full flex flex-col items-center gap-10">
           <TabsList className="rounded-full py-8 sm:py-10 px-2 w-full xs:w-auto justify-start xs:justify-center bg-gray-200 text-gray-600">
-            {categories.map((category) => (
-              <TabsTrigger
-                key={category.value}
-                value={category.value}
-                className="rounded-full p-3 sm:p-5">
-                {category.title}
-              </TabsTrigger>
-            ))}
+            {categories.map((category) =>
+              isRoute ? (
+                <TabsTrigger
+                  key={category.value}
+                  value={category.value}
+                  className="rounded-full p-3 sm:p-5"
+                  onClick={() => handleCategoryChange(category.value)}>
+                  {category.title}
+                </TabsTrigger>
+              ) : (
+                <TabsTrigger
+                  key={category.value}
+                  value={category.value}
+                  className="rounded-full p-3 sm:p-5">
+                  {category.title}
+                </TabsTrigger>
+              )
+            )}
           </TabsList>
+
+          {/* Secondary Tabs for Type */}
+          {isRoute && (
+            <Tabs
+              dir="rtl"
+              defaultValue="image"
+              className="w-full flex flex-col items-center gap-10">
+              <TabsList className="rounded-full py-7 justify-start xs:justify-center bg-gray-200 text-gray-600">
+                <TabsTrigger
+                  className="p-3 rounded-full"
+                  value="image"
+                  onClick={() => handleTypeChange("image")}>
+                  الصور
+                </TabsTrigger>
+
+                <TabsTrigger
+                  className="p-3 rounded-full"
+                  value="video"
+                  onClick={() => handleTypeChange("video")}>
+                  الفيديوهات
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          )}
+          {/* Secondary Tabs for Type */}
 
           {categories.map((category) => (
             <TabsContent
@@ -63,12 +145,20 @@ export default function Works() {
                   لم يتم العثور على أعمال
                 </p>
               )}
+
+              {isRoute ? (
+                renderPagination()
+              ) : (
+                <ButtonComponent
+                  className="mt-12"
+                  text="رؤية المزيد"
+                  href="/works"
+                />
+              )}
             </TabsContent>
           ))}
         </Tabs>
         {/* Tabs */}
-
-        <ButtonComponent className="mt-12" text="رؤية المزيد" href="/works" />
       </div>
     </section>
   );
